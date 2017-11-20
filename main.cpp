31390c4461f76c1df638b45e7f138f5200276117
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-20 14:47:35 -0700

    Updated documentation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 
+// change to a larger uint to sort larger numbers
+using _uint_ = uint32_t;
+
 bool validateInputs(int const & argc) {
     if (argc < 1) {
         std::cout << "No cmd arguments provided.\n"
@@ -10,13 +13,18 @@ bool validateInputs(int const & argc) {
     return true;
 }
 
-
-
 // Input Arguments:
-//  [1] Path to an input file
-//  [2] Numbers to be sorted
+//  - Path to an input file
+//  XOR
+//  - Positive numbers to be sorted
 int main(int argc, char **argv) {
     if (!validateInputs(argc)) return EXIT_FAILURE;
+
+    if (argc > 1) {
+        // TODO: sort numbers
+    } else {
+        // TODO: open input file, sort numbers
+    }
 
     return EXIT_SUCCESS;
 }
