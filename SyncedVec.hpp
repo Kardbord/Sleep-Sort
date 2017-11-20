4b1167e3569a5855013817336f3f46ba8587f468
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-20 15:09:56 -0700

    Implemented size, empty, and operator[] for SyncedVec

--- SyncedVec.hpp
+++ SyncedVec.hpp
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <mutex>
+#include <memory>
+#include <exception>
 
 // A thread-safe version of a std::vector<T>
 //
@@ -25,6 +27,32 @@ public:
 
     // No copy assignment allowed
     SyncedVec& operator=(SyncedVec const&) = delete;
+
+    // Returns the size of m_vec
+    uint32_t size() const {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_vec.size();
+    }
+
+    // Test whether or not m_vec is empty
+    bool empty() const {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_vec.empty();
+    }
+
+    // Obtains the item in m_vec[i] and returns a pointer to that item
+    // Returns nullptr if m_vec is empty or if i is out of range
+    std::shared_ptr<T> operator[](int const & i) {
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+        if (i < 0 || i >= m_vec.size()) return nullptr;
+
+        try {
+           return std::make_shared<T>(m_vec.at(i));
+        } catch(std::exception e) {
+            return nullptr;
+        }
+    }
 
 private:
 
