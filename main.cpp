d56696c7607394bb534ed65805234644148ff971
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-22 21:17:50 -0700

    Fixed bug in validateInputs where condition should have been (argc < 2) instead of  (argc < 1)

--- main.cpp
+++ main.cpp
@@ -11,9 +11,10 @@
 using _uint_ = uint32_t;
 
 bool validateInputs(int const & argc) {
-    if (argc < 1) {
+    // 1st cmd arg is the program name
+    if (argc < 2) {
         std::cout << "No cmd arguments provided.\n"
-                  << "Please provide either a filepath or a list of numbers to sort." << std::endl;
+                  << "Please provide a list of numbers to sort." << std::endl;
         return false;
     }
     return true;
