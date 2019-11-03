3a331b184ed87143302d4708410de2f0e8a220e7
TannerKvarfordt <tanner.kvarfordt@gmail.com>
2019-11-03 14:44:59 -0700

    Cleaned up shell script

--- sleep-sort.sh
+++ sleep-sort.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 #set -x
 
-container=( 2 5 4 6 8 10 7 9 3 1 )
-offset=0.05
+FACTOR=0.05
+
+if [ $# -eq 0 ]; then
+    echo -e "No arguments supplied. Provide a list of positive numbers to sort.\n"
+    echo -e "$0\n"
+    echo -e "Usage:\n"
+    echo -e "  $0 [number1] [number2] ..."
+    echo -e "  Example: $0 0 2 1 6 3 8 1"
+    exit 1
+fi
+
+container=( "$@" )
+integer_check='^[0-9]+$'
 
 for i in "${container[@]}"; do
-    if (( i >= 0 )); then
-        sleep "$(echo "$i*$offset" | bc -l)" &&  echo "$i" &
+    if [[ $i =~ $integer_check ]] && [ "$i" -ge 0 ]; then
+        sleep "$(echo "$i*$FACTOR" | bc -l)" && echo "$i" &
+    else
+        echo "skipping invalid input..."
     fi
 done
 
